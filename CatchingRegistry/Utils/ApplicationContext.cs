ca7248008392dcd15695d9681831d3ed055bdd7f
Salnikov Danils <herlego@mail.ru>
2022-12-07 16:05:56 +0500

    Убрал Lazy Loader

--- CatchingRegistry/Utils/ApplicationContext.cs
+++ CatchingRegistry/Utils/ApplicationContext.cs
@@ -12,13 +12,13 @@ namespace CatchingRegistry
         public DbSet<MunicipalContract> MunicipalContracts { get; set; }
         public DbSet<Animal> Animals { get; set; }
         public DbSet<AttachedFile> Files { get; set; }
+        public DbSet<Log> Logs { get; set; }
         public ApplicationContext() => Database.EnsureCreated();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
             optionsBuilder
-                .UseLazyLoadingProxies()
                 .UseSqlite(@$"DataSource={path}\DB\app.db");
         }
     }
